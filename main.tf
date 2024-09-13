8c7ceea24e11d3061e89f1f49ddc2a7288e115ad
rameshgit16 <mo.ramesh16@gmail.com>
2024-09-13 15:20:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,16 +7,37 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+# Create an Internet Gateway for the VPC
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create a route table for public subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id  # Route all internet-bound traffic through the Internet Gateway
+  }
+}
+
 resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
+  
+  # Associate the public subnet with the public route table
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "ap-south-1b"
+  
+  # Associate the public subnet with the public route table
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_subnet" "private" {
